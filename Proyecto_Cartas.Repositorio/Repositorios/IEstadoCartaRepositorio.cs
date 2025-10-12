9b562eaf6cea391f3f5fb670eafab4bf69e9c7bc
Agustin-Carabajal <carabajal.agustin.ezequiel@gmail.com>
2025-10-11 21:52:29 -0300

    EstadoCarta_Repositorio_y_Controller

--- Proyecto_Cartas.Repositorio/Repositorios/IEstadoCartaRepositorio.cs
+++ Proyecto_Cartas.Repositorio/Repositorios/IEstadoCartaRepositorio.cs
@@ -1,4 +1,5 @@
 ﻿using Proyecto_Cartas.BD.Datos.Entidades;
+using Proyecto_Cartas.Shared.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,12 @@ namespace Proyecto_Cartas.Repositorio.Repositorios
 {
     public interface IEstadoCartaRepositorio : IRepositorio<EstadoCarta>
     {
+        Task<EstadoCartaDTO?> CambiarPosicion(int id, string nuevaPosicion);
+        Task<EstadoCartaDTO?> ColocarEnCampo(int usuarioPartidaId, int cartaId);
+        Task<EstadoCartaDTO?> EnviarAlCementerio(int usuarioPartidaId, int cartaId);
+        Task<List<EstadoCartaDTO>> EstadoCartaDeUnUsuario(int usuarioPartidaId);
+        Task<List<EstadoCartaDTO>> FiltrarPosicion(int usuarioPartidaId, string posicion);
+        Task<EstadoCartaDTO?> RobarCarta(int usuarioPartidaId);
+        Task<List<EstadoCartaDTO>> RobarCartasCantidad(int usuarioPartidaId, int cantidad);
     }
 }
